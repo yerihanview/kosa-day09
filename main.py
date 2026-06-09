112d5afd2cb7b889e4434c3f656333f129babce7
yerihanview <yerihan@gmail.com>
2026-06-09 10:59:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-# from langchain_openai import OpenAI
-# llm = OpenAI()
-# llm_response = llm.invoke("안녕!")
-# print(llm_response)
-
-
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
-chat_response = chat_model.invoke("나도 아무거나 잘 먹어. 지금 나이가 들어서 많이 못먹어. 그래서 맛있는거 먹고싶어.")
-# print(chat_response)
-print(chat_response.content)
+
+# subject = "당근"
+
+# chat_response = chat_model.invoke(subject+"에 대해 설명해줘.")
+# print(chat_response.content)
+
+import streamlit as st
+import time
+
+st.title("디지털 글쓰기"+ chat_model.model)
+subject = st.text_input("글 쓸 주제를 입력해주세요.")
+
+if st.button("궁금증 해결해줘~", type="primary", icon="🔍"):
+    with st.spinner("Wait for it...", show_time=True):
+        chat_response = chat_model.invoke(subject+"에 대해 설명해줘.")
+        st.write(chat_response.content)
 
 
